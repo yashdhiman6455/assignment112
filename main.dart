7de79275ec8668c63cf781e7a1f6e0af1e400a5f
yashdhiman6455 <133200874+yashdhiman6455@users.noreply.github.com>
2023-05-12 11:13:38 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,1 +1,10 @@
+import 'package:flutter/material.dart';
+import 'carosuelslider.dart';
+import 'carouselslider2.dart';
 
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MySlider1(),
+  ));
+}
